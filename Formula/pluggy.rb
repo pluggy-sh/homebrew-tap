ca56f08bcb99ebf31db76782d115f48019248d9f
Christian Hansen <64793a1a@gmail.com>
2026-07-02 00:49:37 +0200

    chore: bump pluggy to 0.6.0 (#3)
    Co-authored-by: pluggy-release-bot <release-bot@users.noreply.github.com>

--- Formula/pluggy.rb
+++ Formula/pluggy.rb
@@ -4,28 +4,28 @@
 class Pluggy < Formula
   desc "Single-binary CLI for Minecraft plugin development"
   homepage "https://pluggy.sh"
-  version "0.4.1"
+  version "0.6.0"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/pluggy-sh/pluggy/releases/download/v#{version}/pluggy-darwin-arm64"
-      sha256 "43a35f3958714a3d88a37815047f317be115b60a1c99fece2ca63901b3ff15df"
+      sha256 "452f7805e09ec6511c39b69c336f743db8d3761e77cfb4596702c312e01dc642"
     end
     on_intel do
       url "https://github.com/pluggy-sh/pluggy/releases/download/v#{version}/pluggy-darwin-amd64"
-      sha256 "c45c82b20b8c6c311ae83912fd9420c4504cf856055adeff9e7552a175988d50"
+      sha256 "9e9a558a9fa04ceb9330bc0f1e8a14e732fa2e5c52c4a45961416d3c8c15940d"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/pluggy-sh/pluggy/releases/download/v#{version}/pluggy-linux-arm64"
-      sha256 "c1889ecb8534e8cd64a7ece3429adc19bb74a2b66d22069ed276e1967db5d1f6"
+      sha256 "265a9caf13fea368b054aac6e3f590496161a48ca27801b3333a90bb34303618"
     end
     on_intel do
       url "https://github.com/pluggy-sh/pluggy/releases/download/v#{version}/pluggy-linux-amd64"
-      sha256 "32bd81132ef7a074ca8565c3543ebf4a49d74b86fed7dd81e0c437bc6a29a71a"
+      sha256 "6d945b716ab58ed5cd39454e5346d19976f7170afee9c4fef4e81124e3382880"
     end
   end
 
